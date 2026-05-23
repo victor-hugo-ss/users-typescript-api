e5934bf44f78656569dcaf7027fca6f55777f8dc
victor-hugo-ss <victorabreu2512@gmail.com>
2026-05-23 17:04:45 -0300

    feat: add getUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,21 @@
+import { STATUS_CODES } from "node:http";
+import type { IGetUsersController, IGetUsersRepository } from "./protocols.js";
+
+export class GetUsersController implements IGetUsersController {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+  async handle() {
+    try {
+      const users = await this.getUsersRepository.getUsers();
+
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong",
+      };
+    }
+  }
+}
