28bd793165982ff81e890874e1aee87769802a6c
victor-hugo-ss <victorabreu2512@gmail.com>
2026-05-24 15:18:56 -0300

    feat: add MongoCreateUserRepository

---
+++ src/repositories/create-user/mongo-create-users.ts
@@ -0,0 +1,25 @@
+import type {
+  CreateUserParams,
+  IcreateUserRepository,
+} from "../../controllers/create-user/protocols.js";
+import { MongoClient } from "../../database/mongo.js";
+import type { User } from "../../models/user.js";
+
+export class MongoCreateUser implements IcreateUserRepository {
+  async createUser(params: CreateUserParams): Promise<User> {
+    const { insertedId } = await MongoClient.db
+      .collection("users")
+      .insertOne(params);
+
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .findOne({ _id: insertedId });
+
+    if (!user) {
+      throw new Error("User not created!");
+    }
+
+    const { _id, ...rest } = user;
+    return { id: _id.toHexString(), ...rest };
+  }
+}
