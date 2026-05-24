be15b47e3ebd5db1cab4b9187c0c5afce587a3bc
victor-hugo-ss <victorabreu2512@gmail.com>
2026-05-24 01:10:05 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,17 @@
 import type { IGetUsersRepository } from "../../controllers/get-users/protocols.js";
+import { MongoClient } from "../../database/mongo.js";
 import type { User } from "../../models/user.js";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstname: "Victor",
-        lastname: "Silva",
-        email: "Victorabreu0511@gmail.com",
-        password: "12345678",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
